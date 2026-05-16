281736929bddb01fbc3c78a8b1c347ade00c103d
milatiscript <scriptmilati@gmail.com>
2026-05-16 21:46:54 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,17 +2,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
   const proxy = createProxyMiddleware({
-    target: 'http://103.83.86.162:10985',
+    target: 'http://103.83.86.162:10985', 
     changeOrigin: true,
     ws: true,
-    pathRewrite: {
-      '^/api/proxy': '', // این بخش /api/proxy را حذف می‌کند تا بقیه آدرس به سرور برسد
-    },
     onProxyReq: (proxyReq, req, res) => {
+      // این هدر باعث می‌شود پنل فکر کند شما با دامنه قدیمی وارد شده‌اید
       proxyReq.setHeader('Host', 'cdn.milatimarket.shop');
     },
+    onProxyRes: (proxyRes, req, res) => {
+      // رفع محدودیت‌های احتمالی مرورگر
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    },
     onError: (err, req, res) => {
-      res.status(500).send('Proxy Error: ' + err.message);
+      res.status(500).send('خطا در اتصال به سرور اصلی: ' + err.message);
     }
   });
 
